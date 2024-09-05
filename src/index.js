97eee9779c96e0017256d4dea8498ab564942da4
carlapascobello <maspascobello@su.edu.ph>
2024-09-05 09:30:19 +0800

    Added typewriter plugin and worked on JS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 function displayAdvice(response) {
   console.log(response.data.answer);
 
-  let conversationBoxElement = document.querySelector("#conversation-box");
-  let optionsElement = document.querySelector(".search-suggestions-container");
-  optionsElement.classList.add("hidden");
+  let adviceOutput = document.querySelector("#advice-input");
+  let conversationBox = document.querySelector("#conversation-box");
+  conversationBox.innerHTML = `<div class="user-reply">${adviceOutput.value}</div>`;
+  conversationBox.innerHTML = `<div class="user-reply">${response.data.answer}</div>`;
+}
+
+function uncalled() {
+  new Typewriter("#conversation-box", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1.5,
+    cursor: "│",
+  });
 }
 
 function generateAdvice(event) {
   event.preventDefault();
 
+  //let adviceOutput = document.querySelector("#advice-input");
+  //let conversationBox = document.querySelector("#conversation-box");
+  //conversationBox.innerHTML = `<div class="user-reply">${adviceOutput.value}</div>`;
   let adviceOutput = document.querySelector("#advice-input");
+
+  let optionsElement = document.querySelector(".search-suggestions-container");
+  optionsElement.classList.add("hidden");
 
   let apiKey = "cfdo22bb3e4ct447ead942a949a20eb3";
   let prompt = `User instruction: ${adviceOutput.value}`;
